30ab21ae4167e1e3fc54312d0870f07d0e683fe7
kir7741 <typhoonjoe2003@hotmail.com>
2017-07-18 14:27:16 +0800

    update

--- js/religion.js
+++ js/religion.js
@@ -1,9 +1,11 @@
 var map;//記錄地圖
 var data = "";//記錄取得的資料
 var markers = [];//記錄載入的標記
+var tableData = [];//記錄載入的表單資料
 var currentInfoWindow = '';//記錄載入的說明視窗
 
 //監聽select
+var tbody = document.querySelector('.js-tbody');
 var selectList = document.querySelector('#select-list');
 selectList.addEventListener('change', changeSelectList, false);
 
@@ -24,11 +26,12 @@ function getData(){
   	data = JSON.parse(xhr.responseText);
   	for(var i = 0; i < data.length; i++){
   		showmarkers(data[i].wgs84aY,data[i].wgs84aX,data[i].CHR_Name);
-  	}
+    }
 
   }
 }
 
+//放置標記與說明
 function showmarkers (lat,lng,title) {
   var infowindow = new google.maps.InfoWindow({
     content: title
@@ -51,6 +54,7 @@ function showmarkers (lat,lng,title) {
   markers.push(marker);
 }
 
+//Select改變時，更新標記與表單資料
 function  changeSelectList (e){
 	for(var i = 0; i < markers.length; i++){
     markers[i].setMap(null);   
@@ -60,19 +64,39 @@ function  changeSelectList (e){
   for(var i = 0; i < data.length; i++){
     if(data[i].CHR_Area == e.target.value){
       showmarkers(data[i].wgs84aY,data[i].wgs84aX,data[i].CHR_Name);
+      var dict = {
+        area : data[i].CHR_Area,
+        name : data[i].CHR_Name,
+        address : data[i].CHR_Address
+      }
+      tableData.push(dict);
+      console.log(tableData);
+      updateTable(tableData);
     }
   }
+}
+
+function updateTable (items) {
+  var str = "";
+  for (var i = 0; i < items.length; i++){
+    str += '<tr><td>' + items[i].area + '</td><td>' + items[i].name + '</td><td>' + items[i].address + '</td></tr>'
+  }
+  console.log(tbody);
+  tbody.innerHTML = str;
 }
 
 
 
 $(document).ready(function(){
+  $('#content').hide();
   $('#select-list').on('change', function(e){
     $('#content').show();
+    google.maps.event.trigger(map, 'resize');
+    map.setCenter({lat: 25.0756191, lng: 121.5788063});
     //var target = e.target.dataset.target;
     //var target = document.getElementById('select-list').dataset.target;
     var target = $(this).attr('data-target'); 
     var offset = $(target).offset();
-    $('html,body').animate({scrollTop : offset.top}, 1500);
+    $('html,body').animate({scrollTop : offset.top}, 1000);
   });
 });
